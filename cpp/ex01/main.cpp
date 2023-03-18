9d1d34e73f6ea379eee08cdde662561d2e31945c
Anas Bouzanbil <abouzanb@e1r2p10.1337.ma>
2023-03-19 00:32:03 +0100

    nothing t do

--- cpp/ex01/main.cpp
+++ cpp/ex01/main.cpp
@@ -6,35 +6,42 @@
 /*   By: abouzanb <abouzanb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/17 14:22:15 by abouzanb          #+#    #+#             */
-/*   Updated: 2023/03/18 16:25:44 by abouzanb         ###   ########.fr       */
+/*   Updated: 2023/03/19 00:27:23 by abouzanb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "phone_number.hpp"
-
 int main()
 {
 	std::string	str;
 	PhoneBook	my_contact;
 	int			i;
 
+	i = 0;
 	my_contact.size = 0;
 	while (1)
 	{
-		if (i == 8)
-			i = 0;
-		std::cout << "\nplease emnter a choice  : ";
-		std::cin >> str;
+
+		std::cout << "\nplease enter a choice  : ";
+		std::getline(std::cin, str);
 		if (str == "add")
-		{
+		{		
+			if (i == 7)
+			i = 0;
 			my_contact.add(i);
 			if (my_contact.size == 7)
 				my_contact.size = 7;
 			else 
 				my_contact.size++;
-		}
+				i++;
+			}
 		else if (str == "search")
 			my_contact.search();
-		i++;
+		else if (str == "exit")
+			exit(0);
+		// else
+		// 	std::cout << "\e[0;31minvalid arguments\e[0m";
+		
+
 	}
 }
--- cpp/ex01/phone_number.cpp
+++ cpp/ex01/phone_number.cpp
@@ -6,83 +6,80 @@
 /*   By: abouzanb <abouzanb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/17 14:28:44 by abouzanb          #+#    #+#             */
-/*   Updated: 2023/03/18 16:28:34 by abouzanb         ###   ########.fr       */
+/*   Updated: 2023/03/19 00:30:32 by abouzanb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "phone_number.hpp"
+#include <stdlib.h>
 
 void PhoneBook::add(int i)
 {
-    std::string str;
+	std::string str;
+	std::cout << "\n";
+	std::cout << "\e[0;30mPlease enter the first name : \e[1;30m";
+	std::cin >> str;
+	account[i].set_first_name(str);
+	
+	std::cout << "\e[0;30mPlease enter the last name : \e[1;30m";
+	std::cin >> str;
+	account[i].set_second_name(str);
+	
+	std::cout << "\e[0;30mPlease enter the nickname : \e[1;30m";
+	std::cin >> str;
+	account[i].set_nickname_name(str);
 
-    std::cout << "Please enter the first name : ";
-    std::cin >> str;
-    account[i].set_first_name(str);
-    
-    std::cout << "Please enter the last name : ";
-    std::cin >> str;
-    account[i].set_second_name(str);
-    
-    std::cout << "Please enter the nickname : ";
-    std::cin >> str;
-    account[i].set_nickname_name(str);
+	std::cout << "\e[0;30mPlease enter the phone  number : \e[1;30m";
+	std::cin >> str;
+	account[i].set_phone_number(str);
 
-    std::cout << "Please enter the phone  number : ";
-    std::cin >> str;
-    account[i].set_phone_number(str);
-    
-    
-    std::cout << "Please enter the darkest_secrete : ";
-    std::cin >> str;
-    account[i].set_secrete(str);
+	std::cout << "\e[0;30mPlease enter the darkest_secrete : \e[1;30m";
+	std::cin >> str;
+	std::cout << "\e[0m";
+	account[i].set_secrete(str);
 }
 void print_colo()
 {
-    std::cout << std::setw(10) << "Index" << std::setw(2) << "|"
+	std::cout << std::setw(10) << "Index" << std::setw(2) << "|"
 				<< std::setw(12) << "First name" << std::setw(2) << "|"
 				<< std::setw(10) << "Last name" << std::setw(2) << "|"
 				<< std::setw(10) << "nickname" << std::setw(2) << "|\n";
-	//std::cout << std::setfill('-') << std::setw(40) << "";
 }
 void PhoneBook::print()
 {
-    int i;
-    i = 0;
-    print_colo();
-    while (i < size)
-    {
-        std::cout   << std::setw(10) << i + 1 << std::setw(2) << "|"
-                    << std::setw(10) << account[i].get_first_name() << std::setw(2) << "|"
-                    << std::setw(10) << account[i].get_the_second() << std::setw(2) << "|"
-                    << std::setw(10) << account[i].get_the_nickname() << std::setw(2) << "|\n";
-       // std::cout << std::setfill('-') << std::setw(40) << "\n";
-        i++;
-    }
+	int i;
+	i = 0;
+	print_colo();
+	while (i < size)
+	{
+		std::cout   << std::setw(10) << i + 1 << std::setw(2) << "|"
+					<< std::setw(10) << account[i].get_first_name() << std::setw(2) << "|"
+					<< std::setw(10) << account[i].get_the_second() << std::setw(2) << "|"
+					<< std::setw(10) << account[i].get_the_nickname() << std::setw(2) << "|\n";
+		i++;
+	}
 }
 
 void PhoneBook::print_index(int i)
 {
-    print_colo();
-        std::cout   << std::setw(10) << i + 1 << std::setw(2) << "|"
-                    << std::setw(12) << account[i].get_first_name() << std::setw(2) << "|"
-                    << std::setw(10) << account[i].get_the_second() << std::setw(2) << "|"
-                    << std::setw(10) << account[i].get_the_nickname() << std::setw(2) << "|\n";
-        std::cout << std::setfill('-') << std::setw(40) << "";
+	print_colo();
+		std::cout   << std::setw(10) << i << std::setw(2) << "|"
+					<< std::setw(12) << account[i].get_first_name() << std::setw(2) << "|"
+					<< std::setw(10) << account[i].get_the_second() << std::setw(2) << "|"
+					<< std::setw(10) << account[i].get_the_nickname() << std::setw(2) << "|\n";
 }
 void PhoneBook::search()
 {
-    int i;
-        print();
-        std::cout << "please choose an index : ";
-    std::cin >> i;
-    std::cout << "{";
-    std::cout << i;
-        std::cout << "}\n";
-    if (i > size)
-    {
-        std::cout << " the index that you entered is invalid\n";
-        return ;
-    }
-    print_index(i);
+	int i;
+
+	i = 0;
+		print();
+		std::cout << "please choose an index : ";
+	std::cin >> i;
+	if (i > size)
+	{
+		std::cout << "\e[0;31m the index that you entered is 1\e[0m\n";
+		return ;
+	}
+	print_index(i);
 }
