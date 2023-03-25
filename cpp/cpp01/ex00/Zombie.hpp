39709a39de0340f10727d1b6408949e83c6765b4
Anas Bouzanbil <abouzanb@e1r11p5.1337.ma>
2023-03-25 23:25:29 +0000

    i wil start ex01

---
+++ cpp/cpp01/ex00/Zombie.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abouzanb <abouzanb@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/25 19:54:45 by abouzanb          #+#    #+#             */
+/*   Updated: 2023/03/25 20:32:08 by abouzanb        ###   ########.fr       */
+/*                                        		                              */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+class Zombie {
+	private :
+		std::string name;
+	public :
+		Zombie(std::string nom);
+		~Zombie();
+		void	announce(void);
+};
+Zombie* newZombie( std::string name );
+void	randomChump( std::string name );
